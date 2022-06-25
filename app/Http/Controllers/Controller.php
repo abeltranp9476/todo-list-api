d54cdd50ff69052f09fa231a4d6a066e29f88c59
Ariel Beltran Paneque <abeltran.9476@gmail.com>
2022-06-25 12:28:43 -0400

    feat: create method responseOk on Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,15 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function responseOk($response)
+    {
+        return response()->json([
+            'data' => $response
+        ]);
+    }
 }
